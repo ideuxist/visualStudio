f06177dc092d57e99f38c7f0b86ff1269c83725d
deuxist <ideuxist@gmail.com>
2022-05-02 00:15:29 +0900

    0502

--- selfstudy/DOM/corona.js
+++ selfstudy/DOM/corona.js
@@ -8,6 +8,56 @@ xhtp.send()
 xhtp.onload = function () {
     let result = JSON.parse(xhtp.responseText)
     init(result);
+    let searchBtn = document.getElementById('search')
+    let fields = ['id', 'facilityName', 'address', 'phoneNumber', 'updatedAt']
+    searchBtn.addEventListener('click', function (e) {
+        e.preventDefault();
+        makeTr(result);
+    })
+}
+
+let xhtp2 = new XMLHttpRequest();
+xhtp2.open('get','../coronaServlet')
+function makeTr(obj){
+    let searchDt = document.getElementById('searchData').value;
+    let tbody = document.getElementById('tbody')
+    tbody.remove();
+    let table = document.getElementById('table')
+    tbody = document.createElement('tbody')
+    tbody.setAttribute('id', 'tbody')
+    table.appendChild(tbody)
+    
+    let filterAry = obj.data.filter(function(elem){ // 필터사용법
+        console.log(elem.sido.indexOf(searchDt))
+       return elem.sido.indexOf(searchDt) !=-1
+        // 매개변수값이 없으면 -1반환
+    })
+    console.log(filterAry);
+    filterAry.forEach(function (elem) {
+        // if (elem.sido == searchDt) {      =>검색값에 입력값이 포함되면 다 검색되게 만들어보자
+        //     let tr = document.createElement('tr');
+        //     tbody.appendChild(tr);
+        //     fields.forEach(function (val) {
+        //         let td = document.createElement('td');
+        //         td.innerHTML = elem[val];
+        //         tr.appendChild(td)
+        //     })
+        // }
+        let tr = document.createElement('tr');
+        tbody.appendChild(tr)
+        fields.forEach(function(val){
+            let td = document.createElement('td');
+            if (val == 'address'){
+                let aTag = document.createElement('a')
+                aTag.setAttribute('href',`daumApi.html?x=${center.lat}&y=${center.lng}`)
+                aTag.innerHTML = elem[val];
+                td.appendChild(aTag);
+            }
+            td.innerHTML = elem[val];
+            tr.appendChild(td)
+
+        })
+    });
 }
 
 function init(jsonData) {
@@ -25,7 +75,15 @@ function init(jsonData) {
         }
     })
 }
-function search(){
+        
+
+
 
     
-}
+
+
+
+
+
+
+
